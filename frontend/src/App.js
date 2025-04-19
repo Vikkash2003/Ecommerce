78c9a4d9e9106c06d785c48c9464cd62b079693f
Vikkash2003 <gowryvikky2003@gmail.com>
2025-04-19 23:08:53 +0530

    home page user interface setup
    Signed-off-by: Vikkash2003 <gowryvikky2003@gmail.com>

---
+++ frontend/src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Home from './pages/home';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <Home/>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
